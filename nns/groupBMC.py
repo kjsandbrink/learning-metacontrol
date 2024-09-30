ec47d0fc3fd3fabff906a9c83df14a07f2d5af2b
Kai Sandbrink <kaijsandbrink@gmail.com>
2024-09-30 13:57:18 +0100

    unified nomenclature task1/task2, improved documentation groupBMC

--- nns/groupBMC.py
+++ nns/groupBMC.py
@@ -1,4 +1,29 @@
 ### COPIED FROM https://github.com/cpilab/group-bayesian-model-comparison/blob/master/groupBMC.py ON 06/04/2024 to allow debugging
+## This script was originally published under teh MIT License
+
+'''
+MIT License
+
+Copyright (c) 2020 cpilab
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
 
 """ Bayesian model selection for group studies.vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/meta-torch.yml vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/mistrain_ape_readouts.py vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/ObserveBetEfficacyTask.py vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/ovb_several_runs.py vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/PeekTakeTorchRNN.py vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/PeekTakeTorchTask.py vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/run_levc.py vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/run_pepe.py vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/sample_ablated_trajectories_levc.py vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/sample_ablated_trajectories_pepe.py vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/sample_levc_trajectories.py vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/sample_mistrained_trajectories.py vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/sample_pepe_trajectories.py vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/sample_perturbed_trajectories_levc.py vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/sample_perturbed_trajectories_pepe.py vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/sample_simulated_perturbed_participants.py vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/save_model_representations.py vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/settings_anal.py vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/test_analyses.py vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/test_case.py vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/trainers.py vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/unittests_model_nosleep.py vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/unittests_model_sleep.py vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/unittests.py vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/utils_project.py vscode-remote://ssh-remote%2B129.67.12.90/home/kai/Documents/Projects/meta-peek-take/utils.py
 Adapted from VBA-toolbox (https://github.com/MBB-team/VBA-toolbox) by Lionel Rigoux.
